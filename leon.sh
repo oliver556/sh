d9c4dbb90fcb1c6e57e8374aaae2d048a5b0fa1c
Jamison <aligoogle@163.com>
2024-05-22 16:41:44 +0800

    fix: - 修复问题

--- leon.sh
+++ leon.sh
@@ -3147,29 +3147,29 @@ while true; do
 							echo "${green}站点信息                      证书到期时间${normal}"
 							echo -e "${cyan}${bold}------------------------------------------------${jiacu}"
 #							ToDo 下面会报错找不到路径文件
-#							for cert_file in /home/web/certs/*_cert.pem; do
-#								domain=$(basename "$cert_file" | sed 's/_cert.pem//')
-#								if [ -n "$domain" ]; then
-#									expire_date=$(openssl x509 -noout -enddate -in "$cert_file" | awk -F'=' '{print $2}')
-#									formatted_date=$(date -d "$expire_date" '+%Y-%m-%d')
-#									printf "%-30s%s\n" "$domain" "$formatted_date"
-#								fi
-#							done
-
-							certs_dir="/home/web/certs"
-							if [ -d "$certs_dir" ]; then
-								find "$certs_dir" -name '*_cert.pem' -type f | while read -r cert_file; do
-								domain=$(basename "$cert_file" | sed 's/_cert.pem//' 2>/dev/null || echo "")
+							for cert_file in /home/web/certs/*_cert.pem; do
+								domain=$(basename "$cert_file" | sed 's/_cert.pem//')
 								if [ -n "$domain" ]; then
-									expire_date=$(openssl x509 -noout -enddate -in "$cert_file" | awk -F'=' '{print \$2}')
+									expire_date=$(openssl x509 -noout -enddate -in "$cert_file" | awk -F'=' '{print $2}')
 									formatted_date=$(date -d "$expire_date" '+%Y-%m-%d')
 									printf "%-30s%s\n" "$domain" "$formatted_date"
 								fi
-								done
-							else
-								echo "找不到证书目录: $certs_dir"
-								echo "找不到 PEM 证书文件。."
-						  	fi
+							done
+
+#							certs_dir="/home/web/certs"
+#							if [ -d "$certs_dir" ]; then
+#								find "$certs_dir" -name '*_cert.pem' -type f | while read -r cert_file; do
+#								domain=$(basename "$cert_file" | sed 's/_cert.pem//' 2>/dev/null || echo "")
+#								if [ -n "$domain" ]; then
+#									expire_date=$(openssl x509 -noout -enddate -in "$cert_file" | awk -F'=' '{print \$2}')
+#									formatted_date=$(date -d "$expire_date" '+%Y-%m-%d')
+#									printf "%-30s%s\n" "$domain" "$formatted_date"
+#								fi
+#								done
+#							else
+#								echo "找不到证书目录: $certs_dir"
+#								echo "找不到 PEM 证书文件。."
+#						  	fi
 
 							echo -e "${cyan}${bold}------------------------------------------------${jiacu}"
 							echo ""
